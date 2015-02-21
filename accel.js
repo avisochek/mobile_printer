d7e43b473f9436374580f48a773d6d2929bedea6
Abhinandan <abhiin1947@gmail.com>
2015-02-21 15:25:54 -0500

    accelerometer feed

---
+++ accel.js
@@ -0,0 +1,22 @@
+/*********************************************
+This basic accelerometer example logs a stream
+of x, y, and z data from the accelerometer
+*********************************************/
+
+var tessel = require('tessel');
+var accel = require('accel-mma84').use(tessel.port['B']);
+
+// Initialize the accelerometer.
+accel.on('ready', function () {
+    // Stream accelerometer data
+  accel.on('data', function (xyz) {
+    console.log('x:', xyz[0].toFixed(2),
+      'y:', xyz[1].toFixed(2),
+      'z:', xyz[2].toFixed(2));
+  });
+
+});
+
+accel.on('error', function(err){
+  console.log('Error:', err);
+});
